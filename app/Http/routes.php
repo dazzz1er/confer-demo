5917ee4517a56358afb18726d98eeb39f6b65044
Dan James Britchford <danbritchford@gmail.com>
2015-05-05 04:58:08 +0100

    Homepage update

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,7 +13,7 @@
 
 Route::get('/', 'WelcomeController@index');
 
-Route::get('home', 'HomeController@index');
+Route::get('demo', 'HomeController@index');
 
 Route::post('userleft', 'HomeController@userloggedout');
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 <html>
 	<head>
-		<title>Laravel</title>
+		<title>Confer demo</title>
 		
 		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
 
@@ -40,8 +40,8 @@
 	<body>
 		<div class="container">
 			<div class="content">
-				<div class="title">Laravel 5</div>
-				<div class="quote">{{ Inspiring::quote() }}</div>
+				<div class="title">Confer demo</div>
+				<div class="quote">The demo is limited to 20 concurrent users. If there is a slot free, you will be allocated one automatically for trial. <a href="/home">Continue</a></div>
 			</div>
 		</div>
 	</body>
