d4cc671151ea485fe884395f070e6a17401b51e6
Dan James Britchford <danbritchford@gmail.com>
2015-05-09 01:19:22 +0100

    Updated home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,16 +2,9 @@
 
 @section('content')
 <div class="container">
-	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
-			<div class="panel panel-default">
-				<div class="panel-heading">Home</div>
-
-				<div class="panel-body">
-					You are logged in!
-				</div>
-			</div>
-		</div>
-	</div>
+	<h1>Welcome to the Confer test site.</h1>
+	<p>Confer is a package for Laravel 5 which makes implementing a real-time chat and messaging service into your website or application a breeze.</p>
+	<p>Testing is simple - start messaging in the global conversation, or create a private chat with another online user. Hey, if you're feeling really wild, why not add some more users to a private conversation and turn it into a group complete with it's own name? Just try not to have too much fun.</p>
+	<p>If you have any questions please feel free to <a href="mailto:danbritchford@gmail.com">contact me</a>.</p>
 </div>
 @endsection
