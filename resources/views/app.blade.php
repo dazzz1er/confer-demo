82e894dddcd5eb86f13043085fd867ba3debc512
Dan James Britchford <danbritchford@gmail.com>
2015-05-14 13:07:22 +0100

    Added the confer bar optional to the demo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -44,6 +44,19 @@
 						<li><a href="{{ url('/auth/login') }}">Login</a></li>
 						<li><a href="{{ url('/auth/register') }}">Register</a></li>
 					@else
+						<li>
+						  <a href="#" data-toggle="dropdown" class="dropdown-toggle" style="position: relative;" id="messages_open_icon"><i class="fa fa-btn fa-envelope"></i></a>
+						  <ul class="dropdown-menu">
+						    <li style="width: 400px; min-height: 40px;">
+						      <ul id="messages_holder_in_bar">
+
+						      @include('confer::barconversationlist')
+
+						      </ul>
+						      <!-- Messages -->
+						    </li>
+						  </ul>
+						</li>
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
 							<ul class="dropdown-menu" role="menu">
